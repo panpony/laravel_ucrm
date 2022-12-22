4e01378a7beba36d21397237ada21d64e2581a7e
panpony <panpony.design@gmail.com>
2022-12-22 18:01:48 +0900

    36~39まで

--- routes/web.php
+++ routes/web.php
@@ -4,6 +4,7 @@ use Illuminate\Foundation\Application;
 use Illuminate\Support\Facades\Route;
 use Inertia\Inertia;
 use App\Http\Controllers\InertiaTestController;
+use App\Http\Controllers\ItemController;
 
 /*
 |--------------------------------------------------------------------------
@@ -58,3 +59,11 @@ Route::get('/dashboard', function () {
 
 require __DIR__.'/auth.php';
 
+
+
+/*--------------------------------------------------
+/* CRM
+/*------------------------------------------------*/
+
+Route::resource('items',ItemController::class)
+->middleware(['auth', 'verified']);
